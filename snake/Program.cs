44b2f5947dc9bbd9615efffff516ada96129f8b8
alexkut96 <kutcasha1@gmail.com>
2018-02-12 10:34:55 +0200

    add Struct in Point.cs

--- snake/Program.cs
+++ snake/Program.cs
@@ -7,21 +7,11 @@ namespace snake
         public static void Main(string[] args)
         {
             // 1-st Point
-            Point p1 = new Point();
-
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
-           
+            Point p1 = new Point(1, 3, '*');
             p1.Draw();
 
             // 2-nd Point
-            Point p2 = new Point();
-
-            p2.x = 3;
-            p2.y = 5;
-            p2.sym = '#';
-
+            Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
         }
