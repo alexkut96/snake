7cea3260e18ea4ffee972c6727a270e60c17a9b3
alexkut96 <kutcasha1@gmail.com>
2018-02-16 08:34:00 +0200

    Vertical.cs

--- snake/Program.cs
+++ snake/Program.cs
@@ -11,16 +11,22 @@ namespace snake
     {
         public static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '#');
+            Point p1 = new Point(2, 3, '#');
             p1.Draw();
 
             Point p2 = new Point(3, 5, '@');
             p2.Draw();
 
-            HorizontalLine p = new HorizontalLine(1, 6, 10, '+');
-            p.Drow();
+            HorizontalLine h1 = new HorizontalLine(2, 40, 0, '_');
+            h1.DrawH();
+            HorizontalLine h2 = new HorizontalLine(2, 40, 10, '_');
+            h2.DrawH();
 
 
+            VerticalLine v1 = new VerticalLine(1, 10, 1, '|');
+            v1.DrawY();
+            VerticalLine v2 = new VerticalLine(1, 10, 40, '|');
+            v2.DrawY();
         }
     }
 }
