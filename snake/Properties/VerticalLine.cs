79eff6843e3edaa18e8e2e48bb13a26897d84e01
alexkut96 <kutcasha1@gmail.com>
2018-02-16 08:34:00 +0200

    Vertical.cs

---
+++ snake/Properties/VerticalLine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+namespace snake
+{
+    public class VerticalLine
+    {
+        List<Point> vList;
+        public VerticalLine(int yUp, int yDown, int x, char sym)
+        {
+            vList = new List<Point>();
+            for (int y = yUp; y <= yDown; y++)
+            {
+                Point p = new Point(x, y, sym);
+                vList.Add(p);
+            }
+        }
+
+        public void DrawY()
+        {
+            foreach (Point y in vList)
+            {
+                y.Draw();
+            }
+        }
+    }
+}
